7686ffe9cbb8fc13ceb0725c1b3181ff5800c445
0Sushant01 <0sushantmore00@gmail.com>
2025-06-15 22:17:53 +0530

    create modules and linked database

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    name=models.CharField(max_length=100)
+    no_of_guests=models.IntegerField()
+    bookingdate=models.DateField()
+
+    def __str__(self):
+        return self.name 
+
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10 ,decimal_places=2)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
